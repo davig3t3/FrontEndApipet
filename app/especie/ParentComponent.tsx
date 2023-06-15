352b0a817ea59f5ea60b4bb3d8180e8d56008d16
davig3t3 <davig3t3@gmail.com>
2023-06-15 10:48:58 -0500

    create page and layouts

---
+++ app/especie/ParentComponent.tsx
@@ -0,0 +1,16 @@
+/* @jsxImportSource @emotion/react */
+
+import React from 'react';
+import EspeciePage from './page';
+
+const ParentComponent = () => {
+  return (
+    <div>
+      {/* Otros componentes */}
+      <EspeciePage />
+      {/* Otros componentes */}
+    </div>
+  );
+};
+
+export default ParentComponent;
